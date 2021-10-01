a8bd0545683781c708b916c1b28c59e513aa8934
Connor Cowan <ccowan003@comcast.net>
2021-10-01 16:29:23 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import App from "./App";
 import usersReducer from "./features/users/usersSlice";
 
 // add imports and code
+const store = createStore(usersReducer);
 
 ReactDOM.render(
   // add imports and code
-  <App />,
-  // add imports and code
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById("root")
 );
